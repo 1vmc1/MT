1acd809da79c6289ed7a944a6923333be59eb429
1vmc1 <vmc0@mail.ru>
2026-01-11 12:59:58 +0300

    Update app.sh

--- app.sh
+++ app.sh
@@ -1,2 +1,20 @@
 #!/bin/bash
-echo "Application deployed successfully"
+set -e
+APP_DIR=/opt/app
+
+# Настройка прав на папку
+sudo chown -R ubuntu:ubuntu $APP_DIR
+
+# Создание venv и установка библиотек (если еще нет)
+cd $APP_DIR
+python3 -m venv .venv
+./.venv/bin/pip install --upgrade pip
+./.venv/bin/pip install -r requirements.txt
+
+# Копирование и запуск сервисов
+sudo cp systemd/*.service /etc/systemd/system/
+sudo systemctl daemon-reload
+sudo systemctl enable music-api music-bot
+sudo systemctl restart music-api music-bot
+
+echo "Services started successfully!"
